c81420e395d02db98ecef30b1600efcdc59751c7
dymafr <32801166+dymafr@users.noreply.github.com>
2022-11-25 12:09:32 +0100

    1 file modified

--- src/app/color.directive.ts
+++ src/app/color.directive.ts
@@ -4,37 +4,35 @@ import {
   HostBinding,
   HostListener,
   Input,
-  OnInit
-} from "@angular/core";
+  OnInit,
+} from '@angular/core';
 
 @Directive({
-  selector: "[appColor]"
+  selector: '[appColor]',
 })
 export class ColorDirective implements OnInit {
-  @HostListener("mouseenter", ["$event"]) private surligne(e: MouseEvent) {
+  @HostListener('mouseenter', ['$event']) private surligne(e: MouseEvent) {
     console.log(e);
-    this.el.nativeElement.style.color = "red";
+    this.el.nativeElement.style.color = 'red';
   }
 
-  @HostListener("mouseleave") private normal() {
-    this.el.nativeElement.style.color = "black";
+  @HostListener('mouseleave') private normal() {
+    this.el.nativeElement.style.color = 'black';
   }
 
-  @HostListener("window:click", ["$event"]) private test(e: MouseEvent) {
+  @HostListener('window:click', ['$event']) private test(e: MouseEvent) {
     console.log(e);
   }
 
-  @Input() @HostBinding("class") private class: string;
+  @Input() @HostBinding('class') private class!: string;
 
-  @HostBinding("attr.role") private role: string;
+  @HostBinding('attr.role') private role = 'admin';
 
-  @HostBinding("style.backgroundColor") private color: string;
+  @HostBinding('style.backgroundColor') private color = 'orange';
 
   constructor(private el: ElementRef<HTMLLIElement>) {}
 
   ngOnInit() {
     console.log(this.class);
-    this.role = "admin";
-    this.color = "orange";
   }
 }
